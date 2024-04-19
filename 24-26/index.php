1dfbedfbaacc19e692203b3959200b0e845aaa30
ZaharichS <zhlzny.ahkaz@gmail.com>
2024-04-19 23:59:35 +0300

    Complete 2 task

--- 24-26/index.php
+++ 24-26/index.php
@@ -57,5 +57,51 @@
     </form>
     </div>
 </div>
+<div id="phpDiv" style="margin-top: 20px;">
+    <div class="center">
+    <?php
+        // Задание 2
+        echo "<b>Задание № 2</b><br>";
+        $userName = "";
+        $userTel = "";
+        $userTelhome = "";
+        $resultStr = "";
+
+
+        if (isset($_POST['userName']) & isset($_POST['userTel']) & isset($_POST['userTelhome'])) {   
+            if (!empty($_POST['userName']) & !empty($_POST['userTel']) & !empty($_POST['userTelhome'])) {
+            $userName = $_POST['userName'];
+            $userTel = $_POST['userTel'];    
+            $userTelhome = $_POST['userTelhome'];  
+            
+            $resultStr = $userName . ", " . $userTel . ", " . $userTelhome;
+            $myfile = fopen("newfile.txt", "a");
+            fwrite($myfile, $resultStr . "\n");
+            fclose($myfile);
+            
+            header('Location: telephone-IS.php');
+            exit();
+            }
+        }
+        
+        // if (isset($_POST['userName']) & isset($_POST['userTel']) & isset($_POST['userTelhome'])) { 
+        //     $userName = $_POST['userName'];
+        //     $userTel = $_POST['userTel'];    
+        //     $userTelhome = $_POST['userTelhome'];  
+        //  }
+    ?>
+    <form method="POST">
+        <label >Введите данные</label><br>
+        <label for="userName">Ваше имя </label>
+            <input type="text" name="userName" id="" maxlength=15 pattern="[А-Я]{1}[а-я]{4,15}" value="Захар"><br><br>
+        <label for="userTel">Ваш рабочий номер телефона<br /> <small style="font-size: 20px; color: green;">Пример: 1 234 567 89 01</small></label>
+            <input type="tel" name="userTel" id="" maxlength=15 pattern="[0-9]{1} [0-9]{3} [0-9]{3} [0-9]{2} [0-9]{2}" value="1 234 567 89 01"><br><br>
+        <label for="userTel">Ваш домашний номер телефона<br><small style="font-size: 20px; color: green;">Пример: 1 234 567 89 01</small></label>
+            <input type="tel" name="userTelhome" id="" maxlength=15 pattern="[0-9]{1} [0-9]{3} [0-9]{3} [0-9]{2} [0-9]{2}" value="1 234 567 89 01"><br><br>
+            <input type="submit">
+    </form>
+    </div>
+</div>
+
 </body>
 </html>
---
+++ 24-26/telephone-IS.php
@@ -0,0 +1,65 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Контакты</title>
+</head>
+<body>
+<div id="phpDiv" style="margin-top: 20px;">
+    <div class="center">
+    <p class="LtoSecondPart">
+            <a href="index.php">Вернуться обратно</a>
+    </p>
+    <?php
+        $userName = "";
+        $userTel = "";
+        $userTelhome = "";
+        
+        // рассмотрим файл как массив
+        $users = file("newfile.txt", FILE_SKIP_EMPTY_LINES);
+        //print_r($users);
+
+        $userList = [];
+        foreach ($users as $user) {
+            list($userName, $userTel, $userTelhome) = explode(", ", $user);
+            $userList[] = [
+                "name" => $userName,
+                "work_phone" => $userTel,
+                "home_phone" => $userTelhome
+            ];
+        }
+    ?>
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg .tg-60hs{font-size:20px;text-align:left;vertical-align:top}
+.tg .tg-0lax{text-align:left;vertical-align:top}
+</style>
+<table class="tg">
+<thead>
+  <tr>
+    <th class="tg-60hs">№</th>
+    <th class="tg-60hs">Имя</th>
+    <th class="tg-60hs">Рабочий номер телефона</th>
+    <th class="tg-60hs">Домашний номер телефона</th>
+  </tr>
+</thead>
+<tbody>
+<?php $i = 1; foreach ($userList as $user): ?>
+
+  <tr>
+    <td class="tg-0lax"><?= $i ?></td>
+    <td class="tg-0lax"><?= $user["name"] ?></td>
+    <td class="tg-0lax"><?= $user["work_phone"] ?></td>
+    <td class="tg-0lax"><?= $user["home_phone"] ?></td>
+</tr>
+  <?php $i++; endforeach;
+ ?>
+</tbody>
+</table>
+</body>
+</html>
